ac81282f8ad9f4241b6280726c97a1bead73efda
joshbrookens <joshua.brookens@mrsmoysclass.com>
2025-05-19 20:14:53 -0700

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -10,11 +10,13 @@ public class Truck extends RaceCar {
             speed = 2; // speed is half of a car's speed, so truck has twice as much reaction time
             threshold = 250; // This is 250 milliseconds is an acceptable margin of error is
         }
-        //Used in state to identify vehicle as truck or car.
+        /* Used in state to identify vehicle as truck or car. @ return speed
+        */
         public int getSpeed(){
             return speed;
         }
-        //Used in state to calculate whether the person turned within the threshold of time.
+          /* Used in state to calculate whether the person turned within the threshold of time. @ return threshold
+    */
         public int getThreshold(){
             return threshold;
         }
